ea09a448245d39beb6bb6591812d433cc456a9e3
theo <theo@jlab.org>
2024-03-11 18:39:38 -0400

    works with default commend_node_logentry type name assigned

---
+++ src/Plugin/migrate/source/Comment.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\logbooks_migrate\Plugin\migrate\source;
+use Drupal\comment\Plugin\migrate\source\d7\Comment as Commentd7;
+
+/**
+ * Source plugin for comments.
+ *
+ * @MigrateSource(
+ *   id = "logbooks_comment"
+ * )
+ */
+class Comment extends Commentd7 {
+
+
+  protected function nextCid() {
+    return isset($this->configuration['first_cid']) ? $this->configuration['first_cid'] : 0;
+  }
+
+
+  protected function nextNid() {
+    return isset($this->configuration['first_nid']) ? $this->configuration['first_nid'] : 0;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $query = parent::query();
+    var_dump($this->configuration);
+    $query->condition('c.cid',$this->nextCid(),'>');
+    $query->condition('n.nid',$this->nextNid(),'>');
+    $query->orderBy('cid','ASC');
+
+    var_dump($query->__toString());
+
+    return $query;
+
+  }
+
+
+}
